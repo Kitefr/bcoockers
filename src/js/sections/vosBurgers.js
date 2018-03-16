51c55027e1ff5eb2c1a4e227671bb8bdec9b82dc
Kitefr <kitefr@gmail.com>
2018-03-16 14:09:33 +0100

    modif vos burgers

---
+++ src/js/sections/vosBurgers.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    let num = 0;
+
+    $("#icons-vos-burgers button").click(function() {
+        const orderBtn = $("#send-order");
+
+        if ($(this).hasClass("active")) {
+            num -= 1;
+        } else {
+            if (num >= 5) {
+                return;
+            }
+            num += 1;
+        }
+
+        $(this).toggleClass("active");
+        $(this)
+            .next()
+            .toggleClass("active");
+
+        if (num > 0) {
+            orderBtn.prop("disabled", false);
+        } else {
+            orderBtn.prop("disabled", true);
+        }
+
+        if (num <= 1) {
+            $("#num-ingredients").text(num + " ingrédient sélectionné");
+        } else {
+            $("#num-ingredients").text(num + " ingrédients sélectionnés");
+        }
+    });
+});
